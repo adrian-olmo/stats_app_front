a08d95d9a95f9eaebe342494a56248e8d4bdf5c4
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-06-18 13:28:03 +0200

    arreglo rutas

--- src/services/fetchUpdatePlayers.js
+++ src/services/fetchUpdatePlayers.js
@@ -1,17 +1,13 @@
-export const fetchUpdatePlayer = async (id, body /*name, age, matches, debut, team_id, position_id*/) => {
+export const fetchUpdatePlayer = async (id, body) => {
 
     try {
-        console.log(body);
         const urlUpdatePlayer = process.env.REACT_APP_UrlUpdatePlayer + `${id}`
         let result = await fetch(urlUpdatePlayer, {
             method: 'PATCH',
             headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem('session') },
-            body: JSON.stringify(
-                body
-            )
+            body: JSON.stringify(body)
         })
-        console.log(result);
-        return result
+        return result.json()
 
     } catch (error) {
         console.log(error);
